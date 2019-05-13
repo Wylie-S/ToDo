1eabf2127fcca39c40baee4de6a87caf6382a2f0
Sarah Wylie <s.wylie43@gmail.com>
2019-05-13 19:50:37 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return <div className="App">My ToDo App</div>;
+
+  const [value, setValue] = React.useState("");
+
+  const [index, setIndex] = React.useState([]);
+}
+
+export default App;
