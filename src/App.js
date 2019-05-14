ae0bfaff21e6d4b453cd320926399dc506206449
Sarah Wylie <s.wylie43@gmail.com>
2019-05-13 21:58:20 -0400

    need to render as a list but break

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">My ToDo App</div>;
+  /*consider the things i will need as state goes and other app things
+i know i will need an input in order to add text
+i will need a button to submit the text
+i need a way to display the items in a list
+there will be an array of items*/
 
-  const [value, setValue] = React.useState("");
-
-  const [index, setIndex] = React.useState([]);
+  //vaule is the text setting it to an empty string plural task
+  const [tasks, setTasks] = React.useState("");
+  //the array of tasks
+  const [value, setValue] = React.useState([]);
+  /*things below this line will be the UI what is rendered above this 
+line is the functionality of things */
+  function addTask() {
+    // basically a spread of all the task then adding the new one to the array
+    setTasks([...tasks, value]);
+    //wnat the input box to auto empty after submitting the task
+    setValue("");
+  }
+  return (
+    <div>
+      <ol />
+      <li>{tasks} </li>
+      <input value={value} onInput={e => setValue(e.target.value)} />
+      <button onClick={addTask}>Submit</button>
+    </div>
+  );
 }
 
 export default App;
